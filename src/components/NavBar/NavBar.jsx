3037628ae3240df00d130f6e86c2306810779a9e
Shea Murphy <Shea.Murphy@nology.io>
2020-04-20 17:51:37 +0100

    Solution completed

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+﻿import React, { Component } from "react";
+import styles from "./NavBar.module.scss";
+import logo from "../../assets/plate-secondary.png";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className={styles.navFlex}>
+        <div className={styles.navFlex}>
+          <img src={logo} alt="FoodWorm logo" />
+          <h1>MealWorm</h1>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
