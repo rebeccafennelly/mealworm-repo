3bea975cbf5391fe17481514eb375be5b7e7c5f7
Shea Murphy <Shea.Murphy@nology.io>
2020-04-23 10:56:06 +0100

    Code along tests completed

--- src/components/SearchBar/SearchBar.test.js
+++ src/components/SearchBar/SearchBar.test.js
@@ -5,12 +5,32 @@ import { shallow } from 'enzyme';
 
 describe("SearchBar tests", () => {
   let component;
+  let mockFn;
+  let testPlaceholder = "Test placeholder";
 
   beforeEach(() => {
-    component = shallow(<SearchBar />);
+    mockFn = jest.fn();
+    component = shallow(<SearchBar placeholder={testPlaceholder} updateSearchText={mockFn} />);
   })
 
   it('should render', () => {
     expect(component).toBeTruthy();
+  })
+
+  it('should render the correct placeholder', () => {
+    component.find('span.fa').simulate('click');
+    expect(component.find('input').prop('placeholder')).toBe(testPlaceholder);
+  })
+
+  it('should call the parent function on input', () => {
+    component.find('span.fa').simulate('click');
+    component.find('input').simulate('input', { target: { value: 'User input' } })
+    expect(mockFn).toHaveBeenCalled();
+  })
+
+  it('should call the parent function with user input', () => {
+    component.find('span.fa').simulate('click');
+    component.find('input').simulate('input', { target: { value: 'User input' } })
+    expect(mockFn).toHaveBeenCalledWith('User input');
   })
 });
