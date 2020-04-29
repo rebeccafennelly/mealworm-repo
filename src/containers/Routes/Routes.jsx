6a5ebf72c4661616652ff70b2fcd63afb4bf2d91
Shea Murphy <Shea.Murphy@nology.io>
2020-04-29 12:34:13 +0100

    Form created and working to add recipes

--- src/containers/Routes/Routes.jsx
+++ src/containers/Routes/Routes.jsx
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 import { Router } from "@reach/router";
+import { firestore } from "../../firebase";
 
 import Dashboard from "../Dashboard";
 import Cookbook from "../Cookbook";
+import CreateRecipe from "../CreateRecipe";
+
 import NotFound from "../../components/NotFound";
 
 class Routes extends Component {
+  addToCookbook = (recipe) => {
+    firestore
+      .collection("recipes")
+      .doc(recipe.id)
+      .set(recipe)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   render() {
     const { recipes } = this.props;
     return (
       <Router>
-        <Dashboard path="/" recipes={recipes} />
+        <Dashboard
+          path="/"
+          recipes={recipes}
+          addToCookbook={this.addToCookbook}
+        />
         <Cookbook path="cookbook" />
+        <CreateRecipe path="create" addToCookbook={this.addToCookbook} />
         <NotFound default />
       </Router>
     );
